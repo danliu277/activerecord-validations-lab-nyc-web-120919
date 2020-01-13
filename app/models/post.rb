fb28022334421ef11c4638b615338e802009def7
flatironschool <danliu277@gmail.com>
2020-01-13 11:00:27 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+    validates :title, presence:true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validate :click_bait
+
+    def click_bait
+        keyword = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        valid = false
+        if(title)
+            keyword.each do |word|
+                if(title.include?(word))
+                    valid = true
+                end
+            end
+        end
+        if(!valid)
+            errors.add(:title, "Not click bait")
+        end
+    end
 end
